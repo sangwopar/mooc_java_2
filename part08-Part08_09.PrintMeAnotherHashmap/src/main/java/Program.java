dcd0caf43ace25cd0941ae1306f59f37a40ec6f8
sangwopar <sangwopar@gmail.com>
2023-08-29 14:52:53 +0900

    08-08 submit

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -7,4 +7,20 @@ public class Program {
         // Test your program here!
     }
 
+    public static void printValues(HashMap<String, Book> hashMap) {
+        for (String str : hashMap.keySet()) {
+            System.out.println(hashMap.get(str));
+        }
+    }
+
+    public static void printValueIfNameContains(HashMap<String,Book> hashMap, String text) {
+        for (String str : hashMap.keySet()) {
+            if (hashMap.get(str).getName().contains(text)) {
+                System.out.println(hashMap.get(str));
+            }
+        }
+    }
+
+
+
 }
